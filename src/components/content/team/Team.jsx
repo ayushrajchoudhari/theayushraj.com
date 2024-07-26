d8dacf91f7179a553308381ba437c9e9e14e81e7
Ayush Raj <ayushrajchoudhari@gmail.com>
2024-07-26 15:35:39 +0530

    Team Section Done!

--- src/components/content/team/Team.jsx
+++ src/components/content/team/Team.jsx
@@ -3,6 +3,10 @@ import Mate2 from "../../../assets/images/vince-fleming-j3lf-Jn6deo-unsplash.jpg
 import Mate3 from "../../../assets/images/alexander-hipp-iEEBWgY_6lA-unsplash.jpg";
 import Mate4 from "../../../assets/images/jake-nackos-IF9TK5Uy-KI-unsplash.jpg";
 import Mate5 from "../../../assets/images/christina-wocintechchat-com-0Zx1bDv5BNY-unsplash.jpg";
+import Github from "../../../assets/icons/github.svg";
+import Linkedin from "../../../assets/icons/linkedin.svg";
+import Facebook from "../../../assets/icons/facebook.svg";
+import Instagram from "../../../assets/icons/instagram.svg";
 
 const Team = () => {
   return (
@@ -57,9 +61,37 @@ const Team = () => {
           <div className="one col">
             <div className="model-icon">
               <img src={Mate2} alt="" className="mate" />
+
+              <div className="mate-desc">
+                <div className="decs-text">
+                  <h5>Mate #1</h5>
+                  <p>Frontend Dev</p>
+                  <div className="social">
+                    <img src={Github} alt="" className="social-media" />
+                    <img src={Linkedin} alt="" className="social-media" />
+                    <img src={Instagram} alt="" className="social-media" />
+                    <img src={Facebook} alt="" className="social-media" />
+                  </div>
+                </div>
+              </div>
+
             </div>
             <div className="model-icon">
               <img src={Mate3} alt="" className="mate" />
+
+              <div className="mate-desc">
+                <div className="decs-text">
+                  <h5>Mate #2</h5>
+                  <p>Backend Dev</p>
+                  <div className="social">
+                    <img src={Github} alt="" className="social-media" />
+                    <img src={Linkedin} alt="" className="social-media" />
+                    <img src={Instagram} alt="" className="social-media" />
+                    <img src={Facebook} alt="" className="social-media" />
+                  </div>
+                </div>
+              </div>
+
             </div>
           </div>
           <div className="two col">
@@ -69,9 +101,37 @@ const Team = () => {
             </p>
             <div className="model-icon">
               <img src={Mate4} alt="" className="mate" />
+
+              <div className="mate-desc">
+                <div className="decs-text">
+                  <h5>Mate #3</h5>
+                  <p>Manager</p>
+                  <div className="social">
+                    <img src={Github} alt="" className="social-media" />
+                    <img src={Linkedin} alt="" className="social-media" />
+                    <img src={Instagram} alt="" className="social-media" />
+                    <img src={Facebook} alt="" className="social-media" />
+                  </div>
+                </div>
+              </div>
+
             </div>
             <div className="model-icon">
               <img src={Mate5} alt="" className="mate" />
+
+              <div className="mate-desc">
+                <div className="decs-text">
+                  <h5>Mate #4</h5>
+                  <p>UI/UX Designer</p>
+                  <div className="social">
+                    <img src={Github} alt="" className="social-media" />
+                    <img src={Linkedin} alt="" className="social-media" />
+                    <img src={Instagram} alt="" className="social-media" />
+                    <img src={Facebook} alt="" className="social-media" />
+                  </div>
+                </div>
+              </div>
+
             </div>
           </div>
         </div>
